b58a842b90d4326e4680d4bc50518e2ef7a0c3e1
achuthan10 <achuthanwbc@gmail.com>
2023-09-14 18:08:02 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,45 +1,23 @@
-const express = require("express");
+import express, { json } from "express";
 
-const Msg = require("./models/message");
+import errorHandler from "./middleware/errorhandler";
+import connection from "./config/dbConnection";
 
-const errorHandler = require("./middleware/errorhandler");
-const connection = require("./config/dbConnection");
-require("dotenv").config();
-const cors = require("cors");
+import ChatRoute from "./routers/ChatRoute";
+import MessageRoute from "./routers/MessageRoute";
+
+import dotenv from "dotenv";
+import cors from "cors";
+
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use("/api/users", require("./routers/userRoutes"));
+app.use(json());
+dotenv.config();
+
+app.use("/chat", ChatRoute);
+app.use("/message", MessageRoute);
 app.use(errorHandler);
-
-//connection();
-
-const httpServer = require("http").createServer(app);
-
-const io = require("socket.io")(httpServer, {
-  cors: { origin: "*" },
-});
-
-io.on("connection", (socket) => {
-  Msg.find().then((result) => {
-    console.log(result.sort((item) => item.timeOfDelivery));
-    socket.emit("message", result);
-  });
-
-  console.log("a user connected");
-
-  socket.on("message", (msg) => {
-    const message = new Msg({ ...msg });
-    message.save().then(() => {
-      io.emit("message", msg);
-    });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-  });
-});
 
 connection();
 
@@ -49,3 +27,29 @@ const port = 3000;
 httpServer.listen(port, () => {
   console.log("server started and listining on 3000");
 });
+
+//const httpServer = require("http").createServer(app);
+
+// const io = require("socket.io")(httpServer, {
+//   cors: { origin: "*" },
+// });
+
+// io.on("connection", (socket) => {
+//   find().then((result) => {
+//     console.log(result.sort((item) => item.timeOfDelivery));
+//     socket.emit("message", result);
+//   });
+
+//   console.log("a user connected");
+
+//   socket.on("message", (msg) => {
+//     const message = new Msg({ ...msg });
+//     message.save().then(() => {
+//       io.emit("message", msg);
+//     });
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("a user disconnected!");
+//   });
+// });
